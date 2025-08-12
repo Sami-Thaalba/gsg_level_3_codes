4c4bb696f979c9c304a6b040e1473fd16e5b78fd
Sami Thaalba <samithaalba@gmail.com>
2025-08-12 15:24:52 +0200

    Implement recursive activity schedule generator

--- activity_schedules/solution.cpp
+++ activity_schedules/solution.cpp
@@ -1,8 +1,43 @@
-
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define int long long
+vector<string> choices = {"Swimming", "Running ", "Football"};
+int cnt = 0;
+void recur(vector<string> sequence, string prev, int n)
+{
 
-int main() {
-    cout << "Activity Schedules Solution" << endl;
+    if (sequence.size() == n)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            cout << sequence[i] << (i == n - 1 ? "" : " ");
+        }
+        cout << endl;
+        // cnt++; we can do this to count how many schedules or use the formula given
+        return;
+    }
+
+    for (string s : choices)
+    {
+        if (s != prev)
+        {
+            sequence.push_back(s);
+            recur(sequence, s, n);
+            sequence.pop_back();
+        }
+    }
+}
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<string> sequence;
+    recur(sequence, "", n);
+    cnt = 3 * (int)pow(2, n - 1); // formula
+    cout << cnt << endl;
+
     return 0;
 }
